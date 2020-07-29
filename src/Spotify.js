417fa5354a3de443c8e77a0a8d74b5469ba14d9a
mac <mac@macs-MacBook-Pro.local>
2020-07-30 00:16:10 +0100

    Add AlbumList component and interface

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,8 @@
 import SpotifyWrapper from "spotify-wrapper";
 
 const spotify = new SpotifyWrapper({
-  token: "YOUR_TOKEN_HERE",
+  token:
+    "BQCPFYg2s5RW3mfxQbyfD0b2SBXEcgRC0Hjz9TSSEuqpSE6N91FM9Pcx15ykEMOR_hNSNpOGlQ1YtknGV9aaMg5H5BbuZg0LNlS4oMfvw9M045MncMT91RT4fr2Cyy3Lj-F73U8Pu-8-kmvs4XDFQ4a46GGCITMTBGTQuf9TpP4a5gpBjTU8uJLh7D20mHffJepFEg",
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("it is working");
+import spotify from "./Spotify";
+import renderAlbums from "./albumList";
+
+const albums = spotify.search.albums("vintage");
+const albumList = document.getElementById("album-list");
+
+albums.then((data) => renderAlbums(data.albums.items, albumList));
