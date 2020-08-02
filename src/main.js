15da8ce8576ab8c44ed28a50c3476abbba383206
mac <mac@macs-MacBook-Pro.local>
2020-08-02 16:45:44 +0100

    Add AlbumTracks component to layout

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import spotify from "./Spotify";
 import renderAlbums from "./albumList";
+import renderAlbumInfo from "./albumInfo";
+import renderAlbumTracks from "./albumTracks";
 
 const albums = spotify.search.albums("vintage");
 const albumList = document.getElementById("album-list");
 
 albums.then((data) => renderAlbums(data.albums.items, albumList));
+
+const album = spotify.album.getAlbum("4YWK8VDqqpOKc2lTifRMAD");
+const albumInfo = document.getElementById("album-info");
+const albumTracks = document.getElementById("album-tracks");
+
+album
+  .then((data) => renderAlbumInfo(data, albumInfo))
+  .then((data) => renderAlbumTracks(data.tracks.items, albumTracks));
