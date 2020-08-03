6157dbaae6e76650b77df413464fd024bd41e150
mac <mac@macs-MacBook-Pro.local>
2020-08-04 00:33:28 +0100

    Add PlaylistTrigger

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
-import spotify from "./Spotify";
-import renderAlbumInfo from "./albumInfo";
-import renderAlbumTracks from "./albumTracks";
 import searchEnterTrigger from "./searchTrigger";
+import selectAlbumTrigger from "./selectaAlbumTrigger";
+import playListTrigger from "./playlistTrigger";
 
 searchEnterTrigger();
-
-const album = spotify.album.getAlbum("4YWK8VDqqpOKc2lTifRMAD");
-const albumInfo = document.getElementById("album-info");
-const albumTracks = document.getElementById("album-tracks");
-
-album
-  .then((data) => renderAlbumInfo(data, albumInfo))
-  .then((data) => renderAlbumTracks(data.tracks.items, albumTracks));
+selectAlbumTrigger();
+playListTrigger();
